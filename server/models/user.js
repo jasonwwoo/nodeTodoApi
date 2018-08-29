05c384453af903f0aefc9c1ff8cc2d8e607d78e1
Jason Woo <jasonwwoo@gmail.com>
2018-08-29 14:32:49 -0500

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,25 @@ UserSchema.statics.findByToken = function(token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  const User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject("typed password didn't match bcrypted password");
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre("save", function(next) {
   const user = this;
 
