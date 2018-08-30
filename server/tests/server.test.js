80c46397fb1e8d497440dee3cc6eb31f7d55691e
Jason Woo <jasonwwoo@gmail.com>
2018-08-30 14:37:01 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -319,3 +319,28 @@ describe("POST /users/login", () => {
       });
   });
 });
+
+describe("DELETE /users/me/token", () => {
+  it("should remove auth token on logout", done => {
+    //.set("x-auth", users[0].tokens[0].token) //supertest
+    request(app)
+      .delete("/users/me/token")
+      .set("x-auth", users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+
+        User.findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(1);
+            done();
+            console.log(user);
+          })
+          .catch(e => {
+            done(e);
+          });
+      });
+  });
+});
