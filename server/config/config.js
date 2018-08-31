7904736132961ddb79e09b65afdde3639b29c56a
Jason Woo <jasonwwoo@gmail.com>
2018-08-31 00:36:53 -0500

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,18 @@
 var env = process.env.NODE_ENV || "development";
 
-if (env === "development") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/TodoApp";
-} else if (env === "test") {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/TodoAppTest";
+if (env === "development" || env === "test") {
+  const config = require("./config.json");
+  const envConfig = config[env]; //"PORT", "MONGO_URI"
+
+  Object.keys(envConfig).forEach(key => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === "development") {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/TodoApp";
+// } else if (env === "test") {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/TodoAppTest";
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -47,7 +47,7 @@ UserSchema.methods.generateAuthToken = function() {
 
   var access = "auth";
   var token = jwt
-    .sign({ _id: user._id.toHexString(), access }, "abc123")
+    .sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
     .toString();
 
   // user.tokens.push({ access, token });
@@ -63,7 +63,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, "abc123");
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject("Could not find by Token");
   }
